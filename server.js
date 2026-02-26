14dadfa9635bd5c99d941e61cba356f40591b32d
vathsa-ponnappa <lingerivathsa@gmail.com>
2026-02-26 18:12:23 +0530

    initial backend commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Cab Hiring API Running 🚖");
+});
+
+// Sample route
+app.use("/api/test", require("./routes/testRoute"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
