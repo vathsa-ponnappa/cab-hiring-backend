e9d7504fc73126ade5175b16ad6d16df2aca1a15
vathsa-ponnappa <lingerivathsa@gmail.com>
2026-02-27 11:24:07 +0530

    made changes in db to render

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const connectDB = require("./config/db");
+const connectDB = require("./db");
 
 dotenv.config();
 connectDB();
